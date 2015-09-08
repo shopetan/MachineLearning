47b705d6abdc2a9fb0e2bc0e4b35d015734e9207
Shohei Kikuchi <shoppe_shopetan@yahoo.co.jp>
2015-09-08 21:40:45 +0900

    mod accuracy

--- svm/svmOneAgainstRestMoneyJudge.py
+++ svm/svmOneAgainstRestMoneyJudge.py
@@ -51,16 +51,9 @@ output = classifier.predict(predict_data)
 #model = svm.libsvm.fit( np.array( training_data ), np.float64( np.array( training_label ) ), kernel='linear' )
 #output = svm.libsvm.predict( np.array( predict_data ), *model,  **{'kernel' : 'linear'} )
 
-hit = 0
-total = len(output)
-
 for i in range( 0, len(output) ) :
     str = "ok" if( int( predict_label[i] ) == int( output[i] ) ) else "miss"
-    hit += 1
     print "predict_label[%d] = %d ,output[%d] = %d" % (i,int(predict_label[i]),i,int(output[i]))
     
-accuracy = float(hit) / total
-print "hit = %ls" % hit 
-print "total = %ls" % total 
-print "accuracy = %lf" % accuracy
+print accuracy_score(predict_label, output)
 
